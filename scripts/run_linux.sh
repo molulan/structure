51728f9b6fb52aee8110fa2532154aeb00fd5791
molulan <molulan-dev@proton.me>
2026-04-29 05:36:04 +0200

    Update run_linux.sh

--- scripts/run_linux.sh
+++ scripts/run_linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/home/morten/Dev/Projects/structure/target/debug:$LD_LIBRARY_PATH
-cd ../backend && cargo build && cd ../frontend && flutter run -d linux
+cd backend && cargo build && cd ../frontend && flutter run -d linux
